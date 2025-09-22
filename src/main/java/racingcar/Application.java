0f1759c9fc5fb775bec1f08de0ddc7647e46443c
pizzaa0930 <jwonpiseu@gmail.com>
2025-09-22 18:13:44 +0900

    게임 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import game.RacingGame;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingGame game = RacingGame.getInstance();
+        game.run();
+        game.close();
     }
 }
